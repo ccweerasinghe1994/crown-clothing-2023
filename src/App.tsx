fe05ecd8f0a98f82df9f6a7210d481e85fa2b628
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-01-12 10:23:14 +0530

    adding the categories object

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
 import { useState } from 'react';
 
 const App = () => {
-  return <div>Hello World</div>;
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Mens',
+    },
+    {
+      id: 5, 
+      title: "women's",
+    },
+  ];
+  return (
+    <div className='categories-container'>
+      {categories.map(({ title }) => (
+        <div className='category-container'>
+          <div className='category-image' />
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
