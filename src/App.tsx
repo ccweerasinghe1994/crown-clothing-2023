3a516d0c0f5fba4728d33d1697e92bd332e6f9c9
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-01-13 09:58:37 +0530

    let's add Routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Home from './routes/home/home.component';
+import React from 'react';
+
+interface ShopProps {}
+const Shop: React.FC<ShopProps> = () => {
+  return <div>This is the Shop Page</div>;
+};
+
+interface NavigationProps {}
+const Navigation: React.FC<NavigationProps> = () => {
+  return (
+    <>
+      <div>Navigation Page</div>
+      <Outlet />
+    </>
+  );
+};
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
