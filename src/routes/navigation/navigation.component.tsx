ee154cd6b881337c52c17e2c1818ac10c8af3df6
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-01-13 19:44:00 +0530

    adding react router dom

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
+
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
 interface NavigationProps {}
 const Navigation: React.FC<NavigationProps> = () => {
   return (
     <>
       <div className='navigation'>
         <Link className='logo-container' to={'/'}>
-          <div>Logo</div>
+          <CrownLogo className='logo' />
         </Link>
         <div className='nav-links-container'>
           <Link className='nav-link' to='/shop'>
